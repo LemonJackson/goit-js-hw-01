63c7079a017b92383d5558e26277a6db9cbe1cb8
LemonJackson <107397445+LemonJackson@users.noreply.github.com>
2024-01-09 23:08:01 +0200

    homework V1.00

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+    let contentSize = Number.parseFloat(content)
+    let paddingSize = Number.parseFloat(padding) * 2
+    let borderSize = Number.parseFloat(border) * 2
+    return contentSize + paddingSize + borderSize
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
